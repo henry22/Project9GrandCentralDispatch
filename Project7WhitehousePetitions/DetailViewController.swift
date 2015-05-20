d41b6cd05178d5e0465266560ed40dfb8ebc8d15
henry22 <henry811010@gmail.com>
2015-05-20 14:44:20 +0800

    Update the DetailViewController class so that it can draw the petition content in a nice way.

--- Project7WhitehousePetitions/DetailViewController.swift
+++ Project7WhitehousePetitions/DetailViewController.swift
@@ -7,39 +7,22 @@
 //
 
 import UIKit
+import WebKit
 
 class DetailViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
-    var detailItem: AnyObject? {
-        didSet {
-            // Update the view.
-            self.configureView()
-        }
-    }
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail: AnyObject = self.detailItem {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.description
-            }
-        }
+    
+    var webView: WKWebView!
+    //contains the dictionary of data
+    var detailItem: [String: String]!
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.configureView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
