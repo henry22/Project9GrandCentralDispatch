2cb01c266733f5426bd84710740e0077fb0bfd12
henry22 <henry811010@gmail.com>
2015-05-20 15:06:03 +0800

    All that HTML will be combined with the body value from the dictionary, then sent to the web view.

--- Project7WhitehousePetitions/DetailViewController.swift
+++ Project7WhitehousePetitions/DetailViewController.swift
@@ -12,7 +12,7 @@ import WebKit
 class DetailViewController: UIViewController {
     
     var webView: WKWebView!
-    //contains the dictionary of data
+    //Contains the dictionary of data
     var detailItem: [String: String]!
     
     override func loadView() {
@@ -22,6 +22,20 @@ class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        if let body = detailItem["body"] {
+            var html = "<html>"
+            html += "<head>"
+            html += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+            html += "<style> body{ font-size: 150%; } </style>"
+            html += "</head>"
+            html += "<body>"
+            html += body
+            html += "</body>"
+            html += "</html>"
+            //Everything needed to show the page, passed in to the web view's loadHTMLString() method so that it gets loaded.
+            webView.loadHTMLString(html, baseURL: nil)
+        }
     }
 
 }
